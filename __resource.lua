7ac48d83bf8e8f98ad8919732672a0d75b948667
Lusaka33 <33868378+Lusaka33@users.noreply.github.com>
2018-06-24 20:28:28 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Mecano Job'
+description 'Eden Drugs'
 
 version '1.0.0'
 
@@ -12,6 +12,5 @@ client_scripts {
 server_scripts {
   '@mysql-async/lib/MySQL.lua',
   'config.lua',
-  'server/main.lua'
-  
+  'server/main.lua' 
 }
